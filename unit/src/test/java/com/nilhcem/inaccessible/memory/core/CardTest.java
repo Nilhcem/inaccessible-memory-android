48ee14e9eb36e61ae21c371ace614642069c91bf
Gautier Nilhcem <nilhcem@gmail.com>
2014-11-16 15:25:13 +0100

    Test parcelables

--- unit/src/test/java/com/nilhcem/inaccessible/memory/core/CardTest.java
+++ unit/src/test/java/com/nilhcem/inaccessible/memory/core/CardTest.java
@@ -1,5 +1,7 @@
 package com.nilhcem.inaccessible.memory.core;
 
+import android.os.Parcel;
+
 import com.nilhcem.inaccessible.memory.RobolectricGradleTestRunner;
 
 import org.junit.Test;
@@ -10,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(RobolectricGradleTestRunner.class)
 public class CardTest {
 
-    private com.nilhcem.inaccessible.memory.core.Card card = new com.nilhcem.inaccessible.memory.core.Card("title");
+    private Card card = new Card("title");
 
     @Test
     public void should_be_face_down_when_created() {
@@ -32,9 +34,9 @@ public class CardTest {
 
     @Test
     public void should_return_true_when_checking_two_cards_with_similar_names_equality() {
-        com.nilhcem.inaccessible.memory.core.Card card1 = new com.nilhcem.inaccessible.memory.core.Card("same_name");
+        Card card1 = new Card("same_name");
         card1.flip();
-        com.nilhcem.inaccessible.memory.core.Card card2 = new com.nilhcem.inaccessible.memory.core.Card("same_name");
+        Card card2 = new Card("same_name");
 
         assertThat(card1).isNotSameAs(card2);
         assertThat(card1).isEqualTo(card2);
@@ -42,8 +44,26 @@ public class CardTest {
 
     @Test
     public void should_return_false_when_checking_different_cards_names_equality() {
-        com.nilhcem.inaccessible.memory.core.Card card1 = new com.nilhcem.inaccessible.memory.core.Card("name_1");
-        com.nilhcem.inaccessible.memory.core.Card card2 = new com.nilhcem.inaccessible.memory.core.Card("name_2");
+        Card card1 = new Card("name_1");
+        Card card2 = new Card("name_2");
         assertThat(card1).isNotEqualTo(card2);
+    }
+
+
+    @Test
+    public void should_restore_from_parcelables() {
+        // Given
+        Card card = new Card("TITLE");
+        card.flip();
+
+        // When
+        Parcel parcel = Parcel.obtain();
+        card.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+
+        // Then
+        Card fromParcel = Card.CREATOR.createFromParcel(parcel);
+        assertThat(fromParcel.getTitle()).isEqualTo("TITLE");
+        assertThat(fromParcel.isFaceDown()).isFalse();
     }
 }
--- unit/src/test/java/com/nilhcem/inaccessible/memory/core/GameTest.java
+++ unit/src/test/java/com/nilhcem/inaccessible/memory/core/GameTest.java
@@ -1,5 +1,7 @@
 package com.nilhcem.inaccessible.memory.core;
 
+import android.os.Parcel;
+
 import com.nilhcem.inaccessible.memory.RobolectricGradleTestRunner;
 
 import org.junit.Test;
@@ -16,7 +18,7 @@ import static org.mockito.Mockito.when;
 @RunWith(RobolectricGradleTestRunner.class)
 public class GameTest {
 
-    private com.nilhcem.inaccessible.memory.core.Game game = new com.nilhcem.inaccessible.memory.core.Game("Fox");
+    private Game game = new Game("Fox");
 
     @Test
     public void should_initialize_a_game_with_two_similar_cards_for_each_entry() {
@@ -52,7 +54,7 @@ public class GameTest {
     @Test(expected = UnsupportedOperationException.class)
     public void should_throw_exception_when_flipping_an_already_face_up_card() {
         // Given
-        com.nilhcem.inaccessible.memory.core.Card card = mock(com.nilhcem.inaccessible.memory.core.Card.class);
+        Card card = mock(Card.class);
         when(card.isFaceDown()).thenReturn(false);
         game.mCards[0] = card;
 
@@ -68,8 +70,8 @@ public class GameTest {
     @Test
     public void should_return_true_when_all_cards_have_been_found() {
         // Given
-        com.nilhcem.inaccessible.memory.core.Card card1 = mock(com.nilhcem.inaccessible.memory.core.Card.class);
-        com.nilhcem.inaccessible.memory.core.Card card2 = mock(com.nilhcem.inaccessible.memory.core.Card.class);
+        Card card1 = mock(Card.class);
+        Card card2 = mock(Card.class);
         when(card1.isFaceDown()).thenReturn(false);
         when(card2.isFaceDown()).thenReturn(false);
         game.mCards[0] = card1;
@@ -82,11 +84,11 @@ public class GameTest {
     @Test
     public void should_keep_cards_face_up_when_pair_is_found() {
         // Given
-        game = new com.nilhcem.inaccessible.memory.core.Game("fox", "fennec");
-        com.nilhcem.inaccessible.memory.core.Card card1 = new com.nilhcem.inaccessible.memory.core.Card("fox");
-        com.nilhcem.inaccessible.memory.core.Card card2 = new com.nilhcem.inaccessible.memory.core.Card("fennec");
-        com.nilhcem.inaccessible.memory.core.Card card3 = new com.nilhcem.inaccessible.memory.core.Card("fox");
-        com.nilhcem.inaccessible.memory.core.Card card4 = new com.nilhcem.inaccessible.memory.core.Card("fennec");
+        game = new Game("fox", "fennec");
+        Card card1 = new Card("fox");
+        Card card2 = new Card("fennec");
+        Card card3 = new Card("fox");
+        Card card4 = new Card("fennec");
         game.mCards[0] = card1;
         game.mCards[1] = card2;
         game.mCards[2] = card3;
@@ -108,10 +110,10 @@ public class GameTest {
     public void should_set_back_cards_to_face_down_when_pair_is_invalid() {
         // Given
         game = new Game("fox", "cat");
-        com.nilhcem.inaccessible.memory.core.Card card1 = new com.nilhcem.inaccessible.memory.core.Card("fox");
-        com.nilhcem.inaccessible.memory.core.Card card2 = new com.nilhcem.inaccessible.memory.core.Card("cat");
-        com.nilhcem.inaccessible.memory.core.Card card3 = new com.nilhcem.inaccessible.memory.core.Card("fox");
-        com.nilhcem.inaccessible.memory.core.Card card4 = new com.nilhcem.inaccessible.memory.core.Card("cat");
+        Card card1 = new Card("fox");
+        Card card2 = new Card("cat");
+        Card card3 = new Card("fox");
+        Card card4 = new Card("cat");
         game.mCards[0] = card1;
         game.mCards[1] = card2;
         game.mCards[2] = card3;
@@ -127,5 +129,32 @@ public class GameTest {
         assertThat(game.mCards[1].isFaceDown()).isTrue();
         assertThat(game.mCards[2].isFaceDown()).isTrue();
         assertThat(game.mCards[3].isFaceDown()).isTrue();
+    }
+
+    @Test
+    public void should_restore_from_parcelables() {
+        // Given
+        game = new Game("1", "2");
+        Card card1 = new Card("1");
+        Card card2 = new Card("2");
+        Card card3 = new Card("3");
+        Card card4 = new Card("4");
+        game.mCards[0] = card1;
+        game.mCards[1] = card2;
+        game.mCards[2] = card3;
+        game.mCards[3] = card4;
+
+        // When
+        Parcel parcel = Parcel.obtain();
+        game.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+
+        // Then
+        Game fromParcel = Game.CREATOR.createFromParcel(parcel);
+        assertThat(fromParcel.getNbCards()).isEqualTo(4);
+        assertThat(fromParcel.getCardAt(0).getTitle()).isEqualTo("1");
+        assertThat(fromParcel.getCardAt(1).getTitle()).isEqualTo("2");
+        assertThat(fromParcel.getCardAt(2).getTitle()).isEqualTo("3");
+        assertThat(fromParcel.getCardAt(3).getTitle()).isEqualTo("4");
     }
 }

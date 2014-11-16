0de3e6dd985959fe1006426f4a58521db74a45fc
Gautier Nilhcem <nilhcem@gmail.com>
2014-11-16 11:54:47 +0100

    Set content description to the card when initially created

--- app/src/main/java/com/nilhcem/inaccessible/memory/ui/CardView.java
+++ app/src/main/java/com/nilhcem/inaccessible/memory/ui/CardView.java
@@ -3,6 +3,7 @@ package com.nilhcem.inaccessible.memory.ui;
 import android.content.Context;
 import android.view.View;
 
+import com.nilhcem.inaccessible.memory.R;
 import com.nilhcem.inaccessible.memory.core.Card;
 
 public class CardView extends View {
@@ -14,5 +15,14 @@ public class CardView extends View {
         super(context);
         mCard = card;
         mPosition = position;
+        setContentDescription();
+    }
+
+    private void setContentDescription() {
+        if (mCard.isFaceDown()) {
+            setContentDescription(getContext().getResources().getString(R.string.card_content_description_face_down, mPosition + 1));
+        } else {
+            setContentDescription(getContext().getResources().getString(R.string.card_content_description_face_up, mPosition + 1, mCard.getTitle()));
+        }
     }
 }
---
+++ unit/src/test/java/com/nilhcem/inaccessible/memory/ui/CardViewTest.java
@@ -0,0 +1,34 @@
+package com.nilhcem.inaccessible.memory.ui;
+
+import com.nilhcem.inaccessible.memory.R;
+import com.nilhcem.inaccessible.memory.RobolectricGradleTestRunner;
+import com.nilhcem.inaccessible.memory.core.Card;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class CardViewTest {
+
+    @Mock Card card;
+    private CardView view;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        when(card.isFaceDown()).thenReturn(true);
+        view = new CardView(Robolectric.application, card, 0);
+    }
+
+    @Test
+    public void should_have_face_off_content_description_when_created() {
+        Assertions.assertThat(view.getContentDescription()).isEqualTo(view.getContext().getString(R.string.card_content_description_face_down, 1));
+    }
+}
